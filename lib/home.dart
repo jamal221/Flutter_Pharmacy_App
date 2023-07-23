14326227691e47dd5a0a6aa24d22be38b0e07183
jamal azizbeigi <jamal.azizbeigi589@gmail.com>
2023-07-24 00:30:25 +0330

    Add Gridview wrapping old widget to new widget Card widget Define body in home page after appBar block. Listview and call a function in list to generate Card.

--- lib/home.dart
+++ lib/home.dart
@@ -46,6 +46,17 @@ class _homepageState extends State<homepage> {
                 ))
           ],
         ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 5,
+            mainAxisSpacing: 5,
+            children: List.generate(4, (int position) {
+              return GeneratedItems();
+            }),
+          ),
+        ),
         bottomNavigationBar: MyAppNavigator(),
         floatingActionButton: FloatingActionButton(
             onPressed: newFn,
@@ -68,4 +79,38 @@ class _homepageState extends State<homepage> {
 
 void newFn() {
   print("We can manage this button here easily");
+}
+
+Card GeneratedItems() {
+  return Card(
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20))),
+    elevation: 5,
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 60,
+            width: 60,
+            child: Image.network(
+                "https://hermoshop.com/26152-homee_default/%DA%A9%D9%88%D9%84%D9%87-%D9%BE%D8%B4%D8%AA%DB%8C-%D9%87%D8%A7%D9%85%D8%AA%D9%88-%D9%85%D8%AF%D9%84-hb202205-2-36l.jpg"),
+          ),
+          Text(
+            "برند",
+            style: TextStyle(
+              fontFamily: "Vazir",
+              fontSize: 20,
+              color: Colors.redAccent,
+            ),
+          ),
+          Text(
+            "قیمت",
+            style: TextStyle(
+                fontFamily: "Nazanin", fontSize: 15, color: Colors.black87),
+          )
+        ],
+      ),
+    ),
+  );
 }
