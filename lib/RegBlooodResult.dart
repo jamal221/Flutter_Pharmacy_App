ff28a61b1ff0256bd1455eb3895272c637ab24c9
jamal azizbeigi <jamal.azizbeigi589@gmail.com>
2023-08-12 01:48:42 +0330

    Add new command to check texteditor convert texteditor to int by int.parse() try to make an intractive page

--- lib/RegBlooodResult.dart
+++ lib/RegBlooodResult.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'MyAppNavigator.dart';
 import 'home.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
 
 class RegBloodResult extends StatefulWidget {
   const RegBloodResult({super.key});
@@ -9,7 +10,13 @@ class RegBloodResult extends StatefulWidget {
 }
 
 class _RegBloodResultState extends State<RegBloodResult> {
-  TextEditingController _suger = TextEditingController();
+  var _suger = TextEditingController();
+  TextEditingController _urea = TextEditingController();
+  TextEditingController _bun = TextEditingController();
+  TextEditingController _creatin = TextEditingController();
+  TextEditingController _chol = TextEditingController();
+  TextEditingController _trig = TextEditingController();
+  String _msgfinal = '';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,8 +52,17 @@ class _RegBloodResultState extends State<RegBloodResult> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
+                    child: Text(
+                      'Fasting Blood Glucose',
+                      style: TextStyle(
+                          color: Color(0xFFFF5A07),
+                          fontSize: 12,
+                          fontFamily: "Vazir"),
+                    ),
+                  ),
+                  Container(
                     width: 200,
-                    padding: EdgeInsets.only(right: 5, top: 5),
+                    padding: EdgeInsets.only(right: 5, top: 5, left: 5),
                     child: TextFormField(
                       controller: _suger,
                       decoration: InputDecoration(
@@ -54,15 +70,311 @@ class _RegBloodResultState extends State<RegBloodResult> {
                           hintText: "Suger",
                           labelText: "Suger Value"),
                     ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Normal:<99',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                        Text(
+                          'Prediabetic:100-125',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                        Text(
+                          'Diabetic:>126',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
+            ),
+            Container(
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Container(
+                      child: Text(
+                        'Urea',
+                        style: TextStyle(
+                            color: Color(0xFFFF5A07),
+                            fontSize: 12,
+                            fontFamily: "Vazir"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 110),
+                    child: Container(
+                      width: 200,
+                      padding: EdgeInsets.only(right: 5, top: 5, left: 5),
+                      child: TextFormField(
+                        controller: _urea,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Urea",
+                            labelText: "Urea"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          '12-45',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Container(
+                      child: Text(
+                        'BUN',
+                        style: TextStyle(
+                            color: Color(0xFFFF5A07),
+                            fontSize: 12,
+                            fontFamily: "Vazir"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 110),
+                    child: Container(
+                      width: 200,
+                      padding: EdgeInsets.only(right: 5, top: 5, left: 5),
+                      child: TextFormField(
+                        controller: _bun,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "BUN",
+                            labelText: "BUN"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          '5.6-21',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Container(
+                      child: Text(
+                        'Creatinic',
+                        style: TextStyle(
+                            color: Color(0xFFFF5A07),
+                            fontSize: 12,
+                            fontFamily: "Vazir"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 85),
+                    child: Container(
+                      width: 200,
+                      padding: EdgeInsets.only(right: 5, top: 5, left: 5),
+                      child: TextFormField(
+                        controller: _creatin,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Creatinic",
+                            labelText: "Creatinic"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          '0.5-1.4',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Container(
+                      child: Text(
+                        'Total Cholestrol',
+                        style: TextStyle(
+                            color: Color(0xFFFF5A07),
+                            fontSize: 12,
+                            fontFamily: "Vazir"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 46),
+                    child: Container(
+                      width: 200,
+                      padding: EdgeInsets.only(right: 5, top: 5, left: 1),
+                      child: TextFormField(
+                        controller: _chol,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Total Cholestrol",
+                            labelText: "Total Cholestrol"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Desirable:<200',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                        Text(
+                          'Borderline: 200-240',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                        Text(
+                          'High : >240',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Container(
+                      child: Text(
+                        'Triglycerides',
+                        style: TextStyle(
+                            color: Color(0xFFFF5A07),
+                            fontSize: 12,
+                            fontFamily: "Vazir"),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 62),
+                    child: Container(
+                      width: 200,
+                      padding: EdgeInsets.only(right: 5, top: 5, left: 1),
+                      child: TextFormField(
+                        controller: _trig,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Triglycerides",
+                            labelText: "Triglycerides"),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Desirable:<200',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                        Text(
+                          'Borderline: 200-400',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        ),
+                        Text(
+                          'High : >400',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontFamily: "Vazir"),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              child: Text(_msgfinal),
             )
           ],
         ),
         bottomNavigationBar: MyAppNavigator(),
         floatingActionButton: FloatingActionButton(
-            onPressed: newFn,
+            onPressed: () {
+              var newSuger = int.parse(_suger.text);
+              if (newSuger > 250) {
+                setState(() {
+                  _msgfinal = "You have many problem. please leave cocka";
+                });
+              }
+            },
+            // onPressed: newFn(),
             backgroundColor: Colors.black,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -78,42 +390,4 @@ class _RegBloodResultState extends State<RegBloodResult> {
       ),
     );
   }
-}
-
-void newFn() {
-  print("We can manage this button here easily");
-}
-
-Card GeneratedItems() {
-  return Card(
-    shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(20))),
-    elevation: 5,
-    child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 60,
-            width: 60,
-            child: Image.network(
-                "https://hermoshop.com/26152-homee_default/%DA%A9%D9%88%D9%84%D9%87-%D9%BE%D8%B4%D8%AA%DB%8C-%D9%87%D8%A7%D9%85%D8%AA%D9%88-%D9%85%D8%AF%D9%84-hb202205-2-36l.jpg"),
-          ),
-          Text(
-            "برند",
-            style: TextStyle(
-              fontFamily: "Vazir",
-              fontSize: 20,
-              color: Colors.redAccent,
-            ),
-          ),
-          Text(
-            "قیمت",
-            style: TextStyle(
-                fontFamily: "Nazanin", fontSize: 15, color: Colors.black87),
-          )
-        ],
-      ),
-    ),
-  );
 }
